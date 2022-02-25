0fc46c156eaedf49f30f5cbe4615d8897e400a66
njacques47 <nojacques94@gmail.com>
2022-02-25 00:04:54 -0500

    getting familiar with new concepts

---
+++ app.js
@@ -0,0 +1,23 @@
+var commandLineArgs = process.argv;
+//console.log(commandLineArgs);
+
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+//console.log(profileDataArgs);
+
+
+const printProfileData = profileDataArr => {
+  // this
+  for (let i = 0; i < profileDataArr.length; i++) {
+    console.log(profileDataArr[i]);
+  }
+
+  console.log('======================');
+
+  // is the same as this
+  profileDataArr.forEach((profileItem) => {
+    console.log(profileItem)
+  });
+};
+
+printProfileData(profileDataArgs);
+
