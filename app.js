ba2e50cf353adc5c3a79c131524145c4f98d3369
njacques47 <nojacques94@gmail.com>
2022-02-25 00:31:20 -0500

    updated working file to remove un-needed code

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
-var commandLineArgs = process.argv;
-//console.log(commandLineArgs);
-
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-//console.log(profileDataArgs);
-
+const profileDataArgs = process.argv.slice(2);
 
 const printProfileData = profileDataArr => {
+  // This...
+  for (let i = 0; i < profileDataArr.length; i += 1) {
+    console.log(profileDataArr[i]);
+  }
+
+  console.log('================');
+
+  // Is the same as this...
   profileDataArr.forEach(profileItem => console.log(profileItem));
 };
 
