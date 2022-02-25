f51fef39d10519e372fa5512317fefa2e3893fcd
njacques47 <nojacques94@gmail.com>
2022-02-25 00:09:45 -0500

    print dynamic output & cleaned up arrowFn

--- app.js
+++ app.js
@@ -6,17 +6,7 @@ const profileDataArgs = process.argv.slice(2, process.argv.length);
 
 
 const printProfileData = profileDataArr => {
-  // this
-  for (let i = 0; i < profileDataArr.length; i++) {
-    console.log(profileDataArr[i]);
-  }
-
-  console.log('======================');
-
-  // is the same as this
-  profileDataArr.forEach((profileItem) => {
-    console.log(profileItem)
-  });
+  profileDataArr.forEach(profileItem => console.log(profileItem));
 };
 
 printProfileData(profileDataArgs);
