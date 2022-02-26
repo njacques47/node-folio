0214b893e28d17f7d8a81ec19d32d6a454319223
njacques47 <nojacques94@gmail.com>
2022-02-25 19:07:47 -0500

    installed 'inquirer' package and included it in app.js

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js'); // receives exported function from the relative path string written in require()
-const profileDataArgs = process.argv.slice(2);
+const inquirer = require('inquirer');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js'); // receives exported function from the relative path string written in require()
 
-const [name, github] = profileDataArgs;
+// const pageHTML = generatePage(name, github);
 
-fs.writeFile('index.html', generatePage(name, github), err => {
-  if (err) throw err;
+// fs.writeFile('index.html', generatePage(name, github), err => {
+//   if (err) throw err;
 
-  console.log('Portfolio complete! Check out the newly generated index.html to see the output!')
-});
+//   console.log('Portfolio complete! Check out the newly generated index.html to see the output!')
+// });
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
