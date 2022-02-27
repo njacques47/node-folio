88508fbf222a1d4df7010048f977f66726eb121f
njacques47 <nojacques94@gmail.com>
2022-02-26 23:37:47 -0500

    created 'about' section

--- src/page-template.js
+++ src/page-template.js
@@ -1,10 +1,23 @@
+// create the about section
+const generateAbout = aboutText => {
+  if (!aboutText) {
+    return '';
+  }
+  return `
+    <section class="my-3" id="about">
+      <h2 class="text-dark bg-primary p-2 display-inline-block">About Me</h2>
+      <p>${aboutText}</p>
+    </section>
+  `;
+};
+
 module.exports = templateData => {
   console.log(templateData);
 
   // destructure projects and about data from templateData based on their property key names
   // this will create three variables based on data in templateData
   const { projects, about, ...header } = templateData;
-  
+
   return `
   <!DOCTYPE html>
   <html lang="en">
@@ -30,9 +43,11 @@ module.exports = templateData => {
         </nav>
       </div>
     </header>
+    
     <main class="container my-5">
-
+          ${generateAbout(about)}
     </main>
+
     <footer class="container text-center py-3">
       <h3 class="text-dark">&copy; ${new Date().getFullYear()} by ${header.name}</h3>
     </footer>
